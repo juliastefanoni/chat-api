6084599ca1acecc459a258bcce691f96c08da457
juliastefanoni <juliastefanonidev@gmail.com>
2021-04-21 19:03:41 -0300

    chore: initialize project

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  return res.json({
+    message: "Hello World"
+  })
+})
+
+app.post('/users', (req, res) => {
+  return res.json({
+    message: 'User saved'
+  })
+})
+
+app.listen(3333, () => console.log('Server is running on port 3333'))
