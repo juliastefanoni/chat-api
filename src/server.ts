e2779f9c9eeef4c1d699fae32474f7aec9cf4d13
juliastefanoni <juliastefanonidev@gmail.com>
2021-04-21 22:26:30 -0300

    feat: settings route

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,11 @@
 import express from 'express'
 
+import { routes } from './routes'
 import './database'
 
 const app = express()
 
-app.get('/', (req, res) => {
-  return res.json({
-    message: "Hello World"
-  })
-})
-
-app.post('/users', (req, res) => {
-  return res.json({
-    message: 'User saved'
-  })
-})
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333, () => console.log('Server is running on port 3333'))
